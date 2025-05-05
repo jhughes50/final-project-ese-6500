eb2d17ee247e8eef9f5d5a7742dce11bbed40a26
luyingz@seas.upenn.edu <luyingz@seas.upenn.edu>
2025-05-05 18:43:57 -0400

    Revert "Added odom plotting"
    This reverts commit f53d2900d6c937ba64b6197cd389e65c1b2a1c5a.

--- visualizer/build/catkin_generated/safe_execute_install.cmake
+++
@@ -1,5 +0,0 @@
-execute_process(COMMAND "/home/root/ws/src/visualizer/build/catkin_generated/python_distutils_install.sh" RESULT_VARIABLE res)
-
-if(NOT res EQUAL 0)
-  message(FATAL_ERROR "execute_process(/home/root/ws/src/visualizer/build/catkin_generated/python_distutils_install.sh) returned error code ")
-endif()
--- visualizer/src/visualizer/map_node.py
+++ visualizer/src/visualizer/map_node.py
@@ -8,10 +8,7 @@ import rospy
 from sensor_msgs.msg import NavSatFix
 from nav_msgs.msg import Odometry
 from std_msgs.msg import String
-import utm
 
-from scipy.spatial.transform import Rotation
-import numpy as np
 from visualizer.map_app import MapApp
 
 class MapNode:
@@ -25,51 +22,21 @@ class MapNode:
 
         self.zone_num_ = rospy.get_param("/visualizer/map/zone_number")
         self.zone_id_ = rospy.get_param("/visualizer/map/zone_id")
-        # print(self.zone_num_, self.zone_id_)
 
         rospy.Subscriber("/gps", NavSatFix, self.gps_callback)
-        rospy.Subscriber("/Odometry", Odometry, self.odom_callback)
-        rospy.Subscriber("/imu/data", String, self.imu_callback)
+        rospy.Subscriber("/odom", Odometry, self.odom_callback)
 
         self.app_ = MapApp(path, ip = ip, port = port)
         self.app_.run_in_thread()
-        self.imu_orientation = (0, 0, 0, 1)
 
     def gps_callback(self, msg : NavSatFix) -> None:
         lat = msg.latitude
         lon = msg.longitude
-        # print(f"gpslat: {lat}, lon: {lon}")
-        self.app_.update_map(lat,lon, source="gps", popup=f"GPS: {lat}, {lon}")
+
+        self.app_.update_map(lat,lon)
 
     def odom_callback(self, msg : Odometry) -> None:
-        # convert gps to utm
-        gps_x, gps_y = 39.9418626, -75.1991958999
-        # convert to utm
-        utm_x, utm_y, _, _ = utm.from_latlon(gps_x, gps_y)
-        x = msg.pose.pose.position.x
-        y = msg.pose.pose.position.y
-        # print(x, y)
-        # quaternion = (
-        #     msg.pose.pose.orientation.x,
-        #     msg.pose.pose.orientation.y,
-        #     msg.pose.pose.orientation.z,
-        #     msg.pose.pose.orientation.w
-        # )
-        quaternion = self.imu_orientation
-        rot = Rotation.from_quat(quaternion)
-        rotation_matrix = rot.as_matrix()
+        lat, lon = utm.to_latlon(msg.pose.pose.position.x, msg.pose.pose.position.y, self.zone_num_, self.zone_id_)
+        self.app_.update_map(lat, lon)
 
-        # rotate the point
-        rotated_point = rotation_matrix[:2, :2] @ np.array([x, y])
-        # print("rotated_point", rotated_point)
-        # print("utm_x, utm_y", utm_x, utm_y)
-        x = utm_x - rotated_point[0]
-        y = utm_y - rotated_point[1]
-        # print(x, y)
 
-        lat, lon = utm.to_latlon(x, y, self.zone_num_, self.zone_id_)
-        # print(f"odmlat: {lat}, lon: {lon}")
-        self.app_.update_map(lat, lon, source='odom', popup=f"ODOM: {lat}, {lon}")
-
-    def imu_callback(self, msg : String) -> None:
-        self.imu_orientation = msg.orientation.x, msg.orientation.y, msg.orientation.z, msg.orientation.w
